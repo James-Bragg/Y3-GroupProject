20214ea39d1dc8e69d4fce6c03fe18959e14383f
James Bragg <jamesbragg01@outlook.com>
2022-03-04 04:46:52 +0000

    v1.1

--- python.py
+++ python.py
@@ -1,3 +1,4 @@
+from tkinter import W
 import pygame
 import random
 pygame.init()
@@ -6,10 +7,17 @@ class GUIVisualInfo:
     Black = 0, 0, 0
     White = 255, 255 , 255
     Green = 0, 255, 0
-    blue = 0, 0, 255
-    red = 255, 0, 0
-    grey = 128, 128, 128
+    Blue = 0, 0, 255
+    Red = 255, 0, 0
     BackgroundColor = Black
+
+    Gradient = [
+        (128, 128, 128),
+        (160, 160, 160),
+        (192, 192, 192)
+    ]
+
+
 
     Margin = 100
     TopBorder = 150
@@ -30,6 +38,14 @@ class GUIVisualInfo:
         self.DataBlockWidth = round((self.width - self.Margin) / len(lst))
         self.DataBlockWidth = round((self.height - self.TopBorder) / (self.maxVal - self.minVal))
         self.initializeX = self.Margin // 2
+
+
+def draw(drawInfo):
+    drawInfo.window.fill(drawInfo.BackgroundColor)
+    pygame.display.update()
+
+def drawList(drawInfo):
+
 
 
 def GenerateStartingData(n, minVal, maxVal):
@@ -53,16 +69,19 @@ def main():
     lst = GenerateStartingData(n, minVal, maxVal)
     GUI = GUIVisualInfo(800, 600, lst)
 
-    pygame.display.update()
 
     while active:
         clock.tick(165)
 
+        draw(GUI)
+
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 active = False
     
     pygame.quit
 
 if __name__ == '__main__':
     main()
+
+
