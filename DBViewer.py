cb823b275e0c32d856d1029025fb2bc4452ef460
CodeWorkingBes <sebastienhsu@gmail.com>
2022-03-24 16:07:05 +0000

    commit

--- DBViewer.py
+++ DBViewer.py
@@ -15,10 +15,6 @@ cr_button = '#48fee7'
 cr_bg_text = '#0c171d'
 str_title = 'Algorithm Visualizer'
 
-root = tk.Tk()
-root.title(str_title)
-root.geometry('{}x{}'.format(n_width, n_height))
-
 
 #These are the variables to be passed into SQL
 NumbersList = [] #List to append numbers into
@@ -28,29 +24,34 @@ conn = sqlite3.connect('SQLDB.sqlite3')
 c = conn.cursor() #Initialising cursor
 conn.commit()
 
-
-sql =c.execute("SELECT * FROM tblMain")
-#rows = c.fetchall()
-
-
-e = Label(root,width=20, text="ID", borderwidth=2,relief='ridge', anchor="w", bg="yellow") 
-e.grid(row=0,column=0)
-e = Label(root,width=20, text="Numbers Picked", borderwidth=2,relief='ridge', anchor="w", bg="yellow") 
-e.grid(row=0,column=1)
-e = Label(root,width=20, text="Algorithm Choice", borderwidth=2,relief='ridge', anchor="w", bg="yellow") 
-e.grid(row=0,column=2)
-e = Label(root,width=20, text="Time Taken", borderwidth=2,relief='ridge', anchor="w", bg="yellow") 
-e.grid(row=0,column=3)
-i=1
-for data in sql: 
-    for j in range(len(data)):
-        e = Label(root, width=20, text=data[j],borderwidth=2, relief='ridge', anchor='w', fg='black') 
-        e.grid(row=i, column=j) 
-        #e.insert(END, data[j])
-    i=i+1
+def Commit():
+    root = tk.Tk()
+    root.title(str_title)
+    root.geometry('{}x{}'.format(n_width, n_height))
+    
+    sql =c.execute("SELECT * FROM tblMain")
+    #rows = c.fetchall()
 
 
-root.mainloop()
+    e = Label(root,width=20, text="ID", borderwidth=2,relief='ridge', anchor="w", bg="yellow") 
+    e.grid(row=0,column=0)
+    e = Label(root,width=20, text="Numbers Picked", borderwidth=2,relief='ridge', anchor="w", bg="yellow") 
+    e.grid(row=0,column=1)
+    e = Label(root,width=20, text="Algorithm Choice", borderwidth=2,relief='ridge', anchor="w", bg="yellow") 
+    e.grid(row=0,column=2)
+    e = Label(root,width=20, text="Time Taken", borderwidth=2,relief='ridge', anchor="w", bg="yellow") 
+    e.grid(row=0,column=3)
+    i=1
+    for data in sql: 
+        for j in range(len(data)):
+            e = Label(root, width=20, text=data[j],borderwidth=2, relief='ridge', anchor='w', fg='black') 
+            e.grid(row=i, column=j) 
+            #e.insert(END, data[j])
+        i=i+1
+
+    root.mainloop()
+
+
 
 
 
