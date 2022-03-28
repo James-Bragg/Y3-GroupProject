f96596a0d86487e76f13427d2e851f289d9ef0fe
CodeWorkingBes <sebastienhsu@gmail.com>
2022-03-28 01:12:10 +0100

    commit

--- DBViewer.py
+++ DBViewer.py
@@ -24,6 +24,17 @@ conn = sqlite3.connect('SQLDB.sqlite3')
 c = conn.cursor() #Initialising cursor
 conn.commit()
 
+key = 5
+alphabet = "abcde5f6g1hi7jklmn8op9q2rs0tuv3wx4yz." + " "
+def Decrypt(self): #Very basic decryption
+    decrypt = ""
+    for i in self:
+        pos = alphabet.find(i)
+        newpos = (pos - 5) % 38
+        decrypt = decrypt + str(alphabet[newpos])
+        print(decrypt)
+    return(decrypt)
+
 def rgb_hack(rgb):
     return "#%02x%02x%02x" % rgb
 
@@ -33,22 +44,20 @@ def Commit():
     root.config(bg=rgb_hack((0,108,180)))
     root.geometry('{}x{}'.format(n_width, n_height))
     
-    sql =c.execute("SELECT * FROM tblMain")
+    sql =c.execute("SELECT NumbersPicked, AlgorithmChosen, TimeTaken FROM tblMain")
     #rows = c.fetchall()
 
 
-    e = Label(root,width=20, text="ID", borderwidth=0,relief='ridge', fg="white", anchor="w", bg=rgb_hack((0,50,83))) 
-    e.grid(row=0,column=0,pady=5,padx=5)
     e = Label(root,width=20, text="Numbers Picked", borderwidth=0,relief='ridge',fg="white", anchor="w", bg=rgb_hack((0,50,83))) 
-    e.grid(row=0,column=1,pady=5,padx=5)
+    e.grid(row=0,column=0,pady=5,padx=5)
     e = Label(root,width=20, text="Algorithm Choice", borderwidth=0,relief='ridge',fg="white", anchor="w", bg=rgb_hack((0,50,83))) 
-    e.grid(row=0,column=2,pady=5,padx=5)
+    e.grid(row=0,column=1,pady=5,padx=5)
     e = Label(root,width=20, text="Time Taken", borderwidth=0,relief='ridge',fg="white", anchor="w", bg=rgb_hack((0,50,83))) 
-    e.grid(row=0,column=3,pady=5,padx=5)
+    e.grid(row=0,column=2,pady=5,padx=5)
     i=1
     for data in sql: 
         for j in range(len(data)):
-            e = Label(root, width=20, text=data[j],borderwidth=0,bg=rgb_hack((0,50,83)),relief='ridge', anchor='w', fg='white') 
+            e = Label(root, width=20, text=Decrypt(str(data[j])),borderwidth=0,bg=rgb_hack((0,50,83)),relief='ridge', anchor='w', fg='white') 
             e.grid(row=i, column=j,padx=2,pady=2) 
             #e.insert(END, data[j])
         i=i+1
--- database_management.py
+++ database_management.py
@@ -1,9 +1,11 @@
 import sqlite3
+from urllib.parse import ParseResultBytes
 from pygame_visualizer import *
 from Tkinter import *
 import random
 import base64
-
+import hashlib
+from cryptography import fernet
 
 #Sqlite3 Commands
 conn = sqlite3.connect('SQLDB.sqlite3')
@@ -12,11 +14,19 @@ conn.commit()
 
 IDNumbers = ["1","2","3","4","5","6","7","8","9"] #Making ID
 
-#def Encryption(self):
- #       password = self.encode("utf-8")
-  #      encoded = base64.b64encode(password)
-    #    return encoded
 
+#Encryption and decryption
+key = 5
+alphabet = "abcde5f6g1hi7jklmn8op9q2rs0tuv3wx4yz." + " "
+def Encrypt(self): #Very basic encryption
+    encrypt = ""
+    for i in self:
+        pos = alphabet.find(i)
+        newpos = (pos + 5) % 38
+        encrypt = encrypt + str(alphabet[newpos])
+        print(encrypt)
+    return(encrypt)
+    
 def EventCreate(NumbersList, SortList, ElapsedTime):
     ID = random.choice(IDNumbers)
     for row in range(5):
@@ -24,20 +34,17 @@ def EventCreate(NumbersList, SortList, ElapsedTime):
     SQL = c.execute("SELECT EventID FROM tblMain WHERE EventID = " + '"' +str(ID)+ '"')
     Check = c.fetchone()
     if Check == None: #No rows exist it means no identical ID
-        Time = str(ElapsedTime) #Value here until we can take time from maingame
-        AlgorithmPicked = SortList
-        NumbersString = ' '.join([str(x) for x in NumbersList]) #Convert list into a string
+        NumbersString = ' '.join([str(x) for x in NumbersList]) #Convert list into a string #Not working yet but close
         NumbersPicked = NumbersString
-        Record = [ID, NumbersPicked, AlgorithmPicked, Time]
+        AlgorithmPicked = SortList.lower()
+        Time = str(ElapsedTime) #Value here until we can take time from maingame
+        NumberSecure = Encrypt(NumbersPicked)
+        AlgorithmSecure = Encrypt(AlgorithmPicked)
+        TimeSecure = Encrypt(Time)
+        Record = [str(ID), NumberSecure, AlgorithmSecure, TimeSecure]
         c.execute("INSERT INTO tblMain VALUES(?,?,?,?)",Record)
         conn.commit()  
     else:
         pass
-        
-
-def Encryption():
-    pass
-
-def DEncryption(): #Don't really need this
-    pass
+    
 
